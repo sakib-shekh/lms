9652f9b0a131274149692e44a1e116948fc68654
sakib-shekh <sakibshekh9049@gmail.com>
2024-01-07 21:37:55 +0530

    auth

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs'
+export default function page() {
+  return (
+
+      <SignIn/>
+
+  )
+}
---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+import { Sign } from 'crypto'
+export default function page() {
+  return (
+
+        <SignUp/>
+
+  )
+}
