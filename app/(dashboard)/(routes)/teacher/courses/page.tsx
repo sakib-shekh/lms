aa1fb8a1a04d95c12a47d5e45dcf13e5ac4bab6e
sakib-shekh <sakibshekh9049@gmail.com>
2024-01-12 12:15:32 +0530

    prisma

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export const CoursePage=()=>{
-    return (<div>
-        course page!
-    </div>)
-}
+export const CoursePage = () => {
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
+};
 export default CoursePage;
